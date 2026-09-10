031f073f7c5b9ebe92d77bdd51123ef4373c6f1f
Shivangi <shivmay90@gmail.com>
2026-09-10 16:24:45 +0530

    commit deployment issues

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,6 @@ if (fs.existsSync(path.join(frontendDist, 'index.html'))) {
   });
 }
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🍕 Pizzazz API running on http://localhost:${PORT}`);
 });
